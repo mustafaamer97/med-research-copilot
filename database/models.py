c0c072c5c6fb9aa6417a72a6def6b239bc9585b5
mustafaamer97 <mostafa.abbas.amer20@gmail.com>
2026-08-23 15:30:54 +0300

    Update models.py

--- database/models.py
+++ database/models.py
@@ -1,1 +1,19 @@
+from sqlalchemy import Column, Integer, String, Text
+from sqlalchemy.orm import declarative_base
 
+Base = declarative_base()
+
+
+class ResearchProject(Base):
+
+    __tablename__ = "research_projects"
+
+    id = Column(Integer, primary_key=True)
+
+    title = Column(String)
+
+    field = Column(String)
+
+    research_type = Column(String)
+
+    notes = Column(Text)
