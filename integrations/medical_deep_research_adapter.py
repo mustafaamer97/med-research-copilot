0eaf563d30d01c08ced0a54221d55901f15c82ed
mustafaamer97 <mostafa.abbas.amer20@gmail.com>
2026-08-23 19:52:04 +0300

    Update and rename medical_deep_research.py to medical_deep_research_adapter.py

---
+++ integrations/medical_deep_research_adapter.py
@@ -0,0 +1,48 @@
+class MedicalDeepResearchAdapter:
+
+    """
+    Adapter layer between
+    Med Research Copilot
+    and Medical Deep Research engine
+    """
+
+    def __init__(self):
+        self.name = "Medical Deep Research"
+
+
+    def search_literature(self, query):
+
+        """
+        Search medical databases
+        """
+
+        results = []
+
+        # Connection will be added here
+
+        return results
+
+
+
+    def rank_evidence(self, papers):
+
+        """
+        Rank studies according to
+        evidence quality
+        """
+
+        ranked = []
+
+        return ranked
+
+
+
+    def create_evidence_report(self, studies):
+
+        """
+        Generate evidence synthesis
+        """
+
+        report = ""
+
+        return report
