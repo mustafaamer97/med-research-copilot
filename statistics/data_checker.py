2b1c86d7a2c5b2129106affe9373835d00c66416
mustafaamer97 <mostafa.abbas.amer20@gmail.com>
2026-08-23 19:31:58 +0300

    Update data_checker.py

--- statistics/data_checker.py
+++ statistics/data_checker.py
@@ -1,1 +1,33 @@
+import pandas as pd
 
+
+def analyze_dataset(file):
+
+    if file.name.endswith(".csv"):
+
+        df = pd.read_csv(file)
+
+    else:
+
+        df = pd.read_excel(file)
+
+
+    report = {
+
+        "rows": len(df),
+
+        "columns": len(df.columns),
+
+        "missing_values":
+            df.isnull().sum().to_dict(),
+
+        "data_types":
+            df.dtypes.astype(str).to_dict(),
+
+        "summary":
+            df.describe().to_dict()
+
+    }
+
+
+    return df, report
