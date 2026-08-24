55066a78dad0534c3ae27a0c762303755d06fb6b
mustafaamer97 <mostafa.abbas.amer20@gmail.com>
2026-08-24 17:32:08 +0300

    Update ai_guardrails.py

--- ai/ai_guardrails.py
+++ ai/ai_guardrails.py
@@ -1,11 +1,27 @@
 FORBIDDEN_TERMS = [
+
+    # References
     "invent doi",
     "invent pmid",
     "fake reference",
     "fabricate citation",
+    "fabricate reference",
+
+    # Statistics
     "invent statistical result",
     "fake p-value",
-    "fake sample size"
+    "fake sample size",
+
+    # Research misconduct
+    "make up data",
+    "fabricate data",
+    "generate fake dataset",
+    "create fake results",
+
+    # Publications
+    "invent study",
+    "invent clinical trial",
+    "fake publication"
 ]
 
 
@@ -16,7 +32,6 @@ def validate_prompt(prompt):
     for term in FORBIDDEN_TERMS:
 
         if term in text:
-
             return False
 
     return True
