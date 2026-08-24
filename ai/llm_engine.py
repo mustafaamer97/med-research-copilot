8224b388c94044465d564ccb99ddc003e81e87c0
mustafaamer97 <mostafa.abbas.amer20@gmail.com>
2026-08-24 14:30:09 +0300

    Update llm_engine.py

--- ai/llm_engine.py
+++ ai/llm_engine.py
@@ -1,27 +1,29 @@
 import os
-from openai import OpenAI
+import google.generativeai as genai
 
 
-client = OpenAI(
-    api_key=os.getenv("OPENAI_API_KEY")
+genai.configure(
+    api_key=os.getenv(
+        "GEMINI_API_KEY"
+    )
+)
+
+
+model = genai.GenerativeModel(
+    "gemini-2.5-flash"
 )
 
 
 def ask_ai(prompt):
 
-    response = client.chat.completions.create(
-        model="gpt-4.1-mini",
-        messages=[
-            {
-                "role": "system",
-                "content":
-                "You are a medical research assistant."
-            },
-            {
-                "role": "user",
-                "content": prompt
-            }
-        ]
-    )
+    try:
 
-    return response.choices[0].message.content
+        response = model.generate_content(
+            prompt
+        )
+
+        return response.text
+
+    except Exception as e:
+
+        return f"AI Error: {str(e)}"
